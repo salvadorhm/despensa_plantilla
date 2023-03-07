ec3d11e51e6ce4ee367eff2a573f6294061fb48b
Salvador HM <salvadorhm@gmail.com>
2023-03-07 08:40:44 -0600

    UPDATE despensa.py actulizado el nombre de los metodos

--- despensa.py
+++ despensa.py
@@ -6,27 +6,23 @@ class Despensa:  # Clase Despensa
     def __init__(self): # Constructor de la clase
         pass # Inicializa el objeto
 
-    def listarDespensa(self, fecha: str) -> bool: # Metodo para listarDespensa()
+    def listarCompras(self, fecha: str) -> bool: # Metodo para listarDespensa()
         # TODO programar el método listarDespensa()
         # mostrar los registros por la fecha indicada con los siguientes datos:
         # sku, nombre del producto, nombre de la tienda
         return False # Regresa False si ocurrio un error en el metodo # Regresa False si ocurrio un error en el metodo
 
-    def insertarDespensa(self) -> bool: # Metodo para insertarDespesa()
+    def insertarCompra(self) -> bool: # Metodo para insertarDespesa()
         # TODO programar el método insertarDespensa()
         return False # Regresa False si ocurrio un error en el metodo
 
-    def buscarDespensa(self,id:int) -> None: # Metodo para buscarDespesa()
+    def buscarComprasFecha(self,id:int) -> None: # Metodo para buscarDespesa()
         # TODO programar el método buscarDespensa()
         # buscar despensa por id
         return False # Regresa False si ocurrio un error en el metodo
 
-    def borrarDespensa(self,id:int) -> None: # Metodo para borrarDespensa()
+    def borrarCompra(self,id:int) -> None: # Metodo para borrarDespensa()
         # TODO programar el método borrarDespensa()
-        return False # Regresa False si ocurrio un error en el metodo
-
-    def actualizarDespensa(self) -> None: # Metodo para actualizarDespensa()
-        # TODO programar el método actualizarDespensa()
         return False # Regresa False si ocurrio un error en el metodo
 
     def valorMinimoProducto(self,sku:str) -> None: # Metodo para mostrar el valor minimo de un producto
--- productos.py
+++ productos.py
@@ -11,7 +11,7 @@ class Productos:  # Clase Productos
             with open("productos.csv", "r") as file: # Abre el archivo para tener acceso a los registros
                 reader = csv.DictReader(file, delimiter=",") # Crer un objeto reader para leer los registros separandolos por el delimitador ,
                 for row in reader: # Recorre todos los registros encontrados y almacena temporalmente cada uno en row
-                    print(row) # imprime los datos del registro como un diccionario
+                    print(f"Registro: {row}") # imprime los datos del registro como un diccionario
             return True # Regresa True si el metodo se ejecuto correctamente
         except Exception as e: # Atrapa cualquier excepcion
             print(f"Error listarProductos() :{e.args}") # Muestra en consola el error que ocurrio
